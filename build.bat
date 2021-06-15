98aa20acf3c5c4ab978649bacc5446bcf8413e64
Sivakumar Veluswamy <v.siva@live.com>
2021-06-14 20:57:31 -0400

    added windows batch file

---
+++ build.bat
@@ -0,0 +1,6 @@
+echo on
+echo "I am here"
+docker build ./mysql/ -t db
+docker run -it -d -p 3306:3306 --name mysql db
+docker build ./website/ -t web
+docker run -it -d -p 3001:80 --link mysql:db web
